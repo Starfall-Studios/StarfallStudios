c2d2bd3e851e7af9e69dff56fdf48357d7e3269f
crafian <ghostmodeoff@gmail.com>
2023-02-27 22:47:02 +0100

    backend update: minor changes

--- FallenLegends/Backend/PythonBackend/main.py
+++ FallenLegends/Backend/PythonBackend/main.py
@@ -3,6 +3,8 @@ from zones import ZoneManager
 from creatures import CreatureManager
 import api
 
+useApi = False
+
 def main():
     print("Fallen Legends Backend v0.1 starting...")
     zoneManager = ZoneManager()
@@ -11,8 +13,9 @@ def main():
 
     creatureManager = CreatureManager()
 
-    thread = threading.Thread(target=api.startAPI)
-    thread.start()
+    if useApi:
+      thread = threading.Thread(target=api.startAPI)
+      thread.start()
     
     creatureManager.uploadCreatures()
 
