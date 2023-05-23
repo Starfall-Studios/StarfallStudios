b968211327d6f6448fa57a0a13cdcf70d15f73b8
David <daviidsolv@gmail.com>
2023-05-23 18:31:15 +0200

    zonecardview added

--- FallenLegends/AndroidProject/app/src/main/java/es/starfallstudios/fallenlegends/models/ZoneListAdapter.java
+++ FallenLegends/AndroidProject/app/src/main/java/es/starfallstudios/fallenlegends/models/ZoneListAdapter.java
@@ -1,14 +1,14 @@
 package es.starfallstudios.fallenlegends.models;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import java.util.ArrayList;
 
 import es.starfallstudios.fallenlegends.R;
 
@@ -26,12 +26,20 @@ public class ZoneListAdapter extends ArrayAdapter<Zone> {
         Zone zone = getItem(i);
 
         if (view == null) {
-            view = LayoutInflater.from(context).inflate(R.layout.cardview, viewGroup, false);
+            view = LayoutInflater.from(context).inflate(R.layout.zonecardview, viewGroup, false);
         }
 
-        ImageView zoneImage = view.findViewById(R.id.deckview_card_image);
-        TextView zoneName = view.findViewById(R.id.deckview_card_name);
-        TextView zoneDescription = view.findViewById(R.id.deckview_card_health);
+        ImageView zoneImage = view.findViewById(R.id.zoneview_card_image);
+        TextView zoneName = view.findViewById(R.id.zoneview_card_name);
+        TextView zoneDescription = view.findViewById(R.id.zoneview_card_desc);
+        Button zoneButton = view.findViewById(R.id.zoneview_card_button);
+
+        zoneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("ZoneListAdapter", "onClick: " + zone.getName());
+            }
+        });
 
         zoneImage.setImageResource(R.drawable.icon_zones);
         zoneName.setText(zone.getName());
