71b84a764b08673330649baa1e7c87b4a52dd304
crafian <ghostmodeoff@gmail.com>
2023-05-28 15:54:25 +0200

    new fragments

--- FallenLegends/AndroidProject/app/src/main/java/es/starfallstudios/fallenlegends/views/CurrentZone.java
+++ FallenLegends/AndroidProject/app/src/main/java/es/starfallstudios/fallenlegends/views/CurrentZone.java
@@ -9,6 +9,7 @@ import androidx.lifecycle.ViewModelProvider;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,6 +30,9 @@ public class CurrentZone extends Fragment {
     private TextView zoneName;
     private TextView zoneDescription;
     private TextView zoneOwner;
+
+    private Button conquerButton;
+    private Button manageButton;
 
     public CurrentZone() {
         // Required empty public constructor
@@ -53,7 +57,7 @@ public class CurrentZone extends Fragment {
         CurrentZoneViewModel viewModel = new ViewModelProvider(this).get(CurrentZoneViewModel.class);
 
 
-        view.findViewById(R.id.acceptButton).setOnClickListener(View -> {
+        view.findViewById(R.id.conquerButton).setOnClickListener(View -> {
             startActivity(new Intent(getActivity(), GameActivity.class));
         });
 
@@ -66,6 +70,11 @@ public class CurrentZone extends Fragment {
         zoneOwner = view.findViewById(R.id.txt_zone_owner);
         zoneOwner.setText(getResources().getString(R.string.zoneFragment_zoneOwner) + " Nobody");
         zoneName = view.findViewById(R.id.txt_zone_name);
+        manageButton = view.findViewById(R.id.manageButton);
+        conquerButton = view.findViewById(R.id.conquerButton);
+
+        manageButton.setVisibility(View.GONE);
+        conquerButton.setVisibility(View.VISIBLE);
 
         viewModel.getZoneInfo().observe(getViewLifecycleOwner(), zoneInfo -> {
             updateUI(zoneInfo);
@@ -76,7 +85,16 @@ public class CurrentZone extends Fragment {
     }
 
     public void updateUI(ZoneInfo zoneInfo) {
+        if (zoneInfo.getOwnerId().equals(GameManager.getInstance().getPlayer().getUid())) {
+            zoneOwner.setText(getResources().getString(R.string.zoneFragment_zoneOwner) + " You");
+            manageButton.setVisibility(View.VISIBLE);
+            conquerButton.setVisibility(View.GONE);
+        } else if (zoneInfo.getOwnerId().equals("")) {
+            zoneOwner.setText(getResources().getString(R.string.zoneFragment_zoneOwner) + " Nobody");
+        } else {
+            zoneOwner.setText(getResources().getString(R.string.zoneFragment_zoneOwner) + " " + zoneInfo.getOwnerName());
+        }
+
         zoneName.setText(getResources().getString(R.string.zoneFragment_zoneName) + " " + zoneInfo.getZoneName());
-        zoneOwner.setText(getResources().getString(R.string.zoneFragment_zoneOwner) + " " + zoneInfo.getOwnerName());
     }
 }
